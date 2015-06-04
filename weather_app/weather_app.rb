dbd5b4ffb856e0300c9f804f4e8e5cb7515da19b
Nicoal Price <nicoalprice@users.noreply.github.com>
2015-06-04 11:19:27 -0400

    Update weather_app.rb

--- weather_app/weather_app.rb
+++ weather_app/weather_app.rb
@@ -3,7 +3,13 @@ require 'yahoo_weatherman'
 # User inputs zipcode.
 
 puts "What is your zipcode?"
-location = gets.chomp
+zipcode = gets.chomp
+
+# Need to figure out how to simplify code by using this method
+#def get_location(zipcode)
+ #   client = Weatherman::Client.new
+ #   client.lookup_by_location(zipcode)
+#end
 
 # Convert temperature from C to F. Thanks to Skillcrush classmate Mary Huynh.
 
@@ -12,18 +18,35 @@ def temp_converter(temp)
     return temp.to_i
 end
 
-# Accept location as parameter and use weather gem to display the current temperature (F) and conditions for that location.
+# Accept location as parameter and use weather gem to display the current temperature (in Fahrenheit) and conditions for that location.
 
-def current_weather(location)
+
+def current_weather(zipcode)
     client = Weatherman::Client.new
-    temperature = temp_converter(client.lookup_by_location(location).condition['temp'])
-    conditions = client.lookup_by_location(location).condition['text'].downcase
-    puts "The current weather for #{location} is #{temperature} degrees Fahrenheit and #{conditions}."
+    client.lookup_by_location(zipcode)
+    
+    temperature = temp_converter(client.lookup_by_location(zipcode).condition['temp'])
+    conditions = client.lookup_by_location(zipcode).condition['text'].downcase
+    puts "The current weather for #{zipcode} is #{temperature} degrees Fahrenheit and #{conditions}."
 end
 
-current_weather(location)
+current_weather(zipcode)
 
 
 # Spit out a cutesie message about the weather
 
-# Find 5 day forecast
+# Find and display 5 day forecast
+
+def weather_forecast(zipcode)
+    client = Weatherman::Client.new
+    client.lookup_by_location(zipcode)
+    
+    client.lookup_by_location(zipcode).forecasts.each do |forecast|
+        puts forecast['day'].to_s + ' is going to be ' + forecast['text'].downcase + ' with a low of ' + forecast['low'].to_s + ' and a high of ' + forecast['high'].to_s
+    end
+end
+ 
+today = Time.now.strftime('%w').to_i
+
+weather_forecast(zipcode)
+
